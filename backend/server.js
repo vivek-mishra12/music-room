89beb5a3cc3070ccce00b5d39274d2b2af9a8f1b
Vivek Mishra <vivek77056@gmail.com>
2026-06-01 03:35:58 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const axios = require("axios");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://music-room-umber.vercel.app",
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 
 const server = http.createServer(app);
 
