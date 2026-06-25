752cc8134c9163d52a68b3cde07321e80bc2469c
Vivek Mishra <vivek77056@gmail.com>
2026-06-25 10:29:42 +0530

    api

--- backend/routes/geminiRoutes.js
+++ backend/routes/geminiRoutes.js
@@ -3,6 +3,6 @@ const express = require('express');
 const router = express.Router();
 const { recommendSongs } = require('../controllers/geminiController');
 
-router.post('/recommend', recommendSongs);
+router.post('/suggest', recommendSongs);
 
 module.exports = router;
